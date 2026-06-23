6742c2dd4a33f269569dfa1aa03ae96dcef444bc
Héctor Nieto <richtunic@MacBook-Air-de-Hector.local>
2026-06-22 20:55:28 -0700

    chore: bump version to 1.0.1 (code 2) for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         applicationId = "io.kitsux.app"
 
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
